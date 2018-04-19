52e24af53bf8ada200f898cb2fc28c25efa86e1a
lfp1991lfp <128ling@163.com>
2018-04-19 15:19:55 +0800

    1.更换包

---
+++ lfp-base-recycleview-library/src/main/java/com/lfp/lfp_base_recycleview_library/layoutmanager/NoScrollLinearLayoutManager.java
@@ -0,0 +1,36 @@
+package com.lfp.lfp_base_recycleview_library.layoutmanager;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.AttributeSet;
+
+/**
+ * Created by linfp on 2016/4/22.
+ * 单数据的线性布局管理器会崩溃，所以继承重写
+ */
+public class NoScrollLinearLayoutManager extends LinearLayoutManager {
+    private boolean isScrollEnabled = true;
+
+    public NoScrollLinearLayoutManager(Context context) {
+        super(context);
+    }
+
+    public NoScrollLinearLayoutManager(Context context, int orientation, boolean reverseLayout) {
+        super(context, orientation, reverseLayout);
+    }
+
+    public NoScrollLinearLayoutManager(Context context, AttributeSet attrs, int defStyleAttr,
+                                       int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public void setScrollEnabled(boolean flag) {
+        this.isScrollEnabled = flag;
+    }
+
+    @Override
+    public boolean canScrollVertically() {
+        //Similarly you can customize "canScrollHorizontally()" for managing horizontal scroll
+        return isScrollEnabled && super.canScrollVertically();
+    }
+}
